d456517155194446d7ec848cf8f1df20857c807e
Melaverde1987 <79062250+Melaverde1987@users.noreply.github.com>
2025-09-09 00:37:48 +0200

    09.09

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -30,26 +30,15 @@ const swiper = new Swiper('.swiper-js', {
     nextEl: '.swiper-btn-next',
     prevEl: '.swiper-btn-prev',
   },
-
-  /*
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-  */
 });
 
-var reviewSwiper = new Swiper('.mySwiper', {
+var reviewSwiper = new Swiper('.reviews-swiper', {
   slidesPerView: 1,
-  spaceBetween: 30,
+  spaceBetween: 16,
   slidesPerGroup: 1,
   loop: true,
   loopFillGroupWithBlank: true,
   breakpoints: {
-    //  >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 16,
-    },
     //  >= 768px
     768: {
       slidesPerView: 2,
@@ -66,29 +55,4 @@ var reviewSwiper = new Swiper('.mySwiper', {
     el: '.swiper-reviews-pagination',
     clickable: true,
   },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
 });
-
-/*
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: tru
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
-*/
